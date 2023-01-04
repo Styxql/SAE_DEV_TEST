a4adeb5a3083b0f902d64352af8e0850533390a1
Quentin Laval <quentin.laval@etu.univ-smb.fr>
2023-01-04 09:04:29 +0100

    sss

--- SAE_DEV/SAE_DEV/Game1.cs
+++ SAE_DEV/SAE_DEV/Game1.cs
@@ -58,14 +58,13 @@ namespace SAE_DEV
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             _tiledMap = Content.Load<TiledMap>("road");
-            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);           
-            //_textureCar  = Content.Load<Texture2D>("Car");
-            //_textureAmbulance = Content.Load<Texture2D>("Ambulance");
-            //_textureAudi = Content.Load<Texture2D>("Audi");
-            //_textureMiniTruck = Content.Load<Texture2D>("Truck");
-            //_textureTruck = Content.Load<Texture2D>("Minitruck");
-            //_textureMinivan = Content.Load<Texture2D>("Minivan");
-            //_textureVoitureBolide = Content.Load<Texture2D>("Black_viper");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+            _textureCar = Content.Load<Texture2D>("Car");
+            _textureAmbulance = Content.Load<Texture2D>("Ambulance");
+            _textureMiniTruck = Content.Load<Texture2D>("Truck");
+            _textureTruck = Content.Load<Texture2D>("Minitruck");
+            _textureMinivan = Content.Load<Texture2D>("Minivan");
+            _textureVoitureBolide = Content.Load<Texture2D>("Blackviper");
             _textureVoiturePolice = Content.Load<Texture2D>("Police");
 
 
@@ -100,7 +99,9 @@ namespace SAE_DEV
             _tiledMapRenderer.Draw();
             _spriteBatch.Begin();
 
+            _spriteBatch.Draw(_textureVoiturePolice, _positionInitialVoitureEnnemie, Color.White);
             _spriteBatch.Draw(_textureCar, _positionInitialVoitureEnnemie, Color.White);
+
 
 
 
