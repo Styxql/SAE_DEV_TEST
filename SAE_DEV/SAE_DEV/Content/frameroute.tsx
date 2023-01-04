b50ecf663a222ca3575295a8adf5c726bf4ade66
David Lescure <david.lescure@etu.univ-smb.fr>
2023-01-04 08:55:57 +0100

    valid

--- SAE_DEV/SAE_DEV/Content/frameroute.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="frameroute" tilewidth="32" tileheight="32" tilecount="2" columns="1">
- <image source="frameroute.png" width="32" height="64"/>
-</tileset>
